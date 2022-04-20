d4b99375961f0a94b21f7520a20004c3f864730b
Vladimir Mikryukov <v.mikryukov@gmail.com>
2022-04-20 14:26:12 +0300

    fix reintegration script (case with a single randomization chunk)

--- bin/05_reintegrate_basedatas_post_rand.pl
+++ bin/05_reintegrate_basedatas_post_rand.pl
@@ -44,15 +44,17 @@ die "Only one file found using $glob ($files[0])" if @files == 1;
 my $first_bd = shift @files;
 my $bd = Biodiverse::BaseData->new (file => $first_bd);
 
-foreach my $from_file (@files) {
-    say "Reintegrating from $from_file";
+## Reintegrate chunks
+if (scalar(@files) > 1) {
+    foreach my $from_file (@files) {
+        say "Reintegrating from $from_file";
 
-    my $from_bd = Biodiverse::BaseData->new(file => $from_file);
-    $bd->reintegrate_after_parallel_randomisations (
-        from => $from_bd,
-        no_check_groups_and_labels => $no_verify,
-    );
-    
+        my $from_bd = Biodiverse::BaseData->new(file => $from_file);
+        $bd->reintegrate_after_parallel_randomisations (
+            from => $from_bd,
+            no_check_groups_and_labels => $no_verify,
+        );
+    }
 }
 
 $bd->save_to (filename => $opfx, method => 'save_to_sereal');
